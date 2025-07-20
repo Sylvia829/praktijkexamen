1d9d854ebe465d12aed7780a9ef43cac1f53a9e9
sylvia gooskens <sylvia_gooskens@hotmail.com>
2025-07-20 20:00:38 +0200

    nieuwe regels toegevoegd

--- praktijkexamen.py
+++ praktijkexamen.py
@@ -1,4 +1,11 @@
 def bereken_pensioen(leeftijd, statuut):
     basisbedragen = [("medewerker", 350, 20),("zelfstandige", 300, 15),("ambtenaar", 370, 25)]
     for s in basisbedragen:
-        
+        if statuut == s[0]:
+            if leeftijd >70:
+                return s[1] + s[2]
+            elif leeftijd>=65:
+                return s[1]
+            else:
+                return None
+            
